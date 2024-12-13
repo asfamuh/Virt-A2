d5c305da9c0e065393f9e37d50eb0130f1330551
Muhammad <ashfamuh@sheridancollege.ca>
2024-12-13 15:57:36 -0500

    Demo commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 app.get('/', (req, res) => { 
-res.send('Hello, Dockerized Node.js App! I have made a testing change.'); 
+res.send('Hello, Dockerized Node.js App! I have made a change for the demo.'); 
 }); 
 app.listen(PORT, () => { 
 console.log(`App running on http://localhost:${PORT}`); 
